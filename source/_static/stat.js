33cd62db665c908948fc370efd6544201ef4190d
chenfengjin <1871653365@qq.coom>
2021-02-18 11:13:22 +0800

    RFC: Update tongji account to xchain-base

--- source/_static/stat.js
+++ source/_static/stat.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function () {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?5a3ed4896e23e5f077ed81e22be08bfa";
+  hm.src = "https://hm.baidu.com/hm.js?98a36458b129ac8963d1b3bbfcd522bb";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
