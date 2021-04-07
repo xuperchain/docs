afbbb614b6ec2f71298140db15b500acb2182e91
chenfengjin <chenfengjin@baidu.com>
2021-04-07 17:19:01 +0800

    RFC: Add PR Review

--- scripts/pr.sh
+++ scripts/pr.sh
@@ -1,10 +1,15 @@
-rm -rf pr
-for pr in `git ls-remote   |awk '{print $2}'  |grep -v merge |sort -n  -t / -k 3 |tail -n 10 `;do 
-    git fetch origin $pr
-    git checkout $pr
-    dir=pr/`echo $pr|awk -F '/' '{print $3}'`
-    make html;mkdir -p dir
-    mv build/html/* dir/
-done
-git checkout master
+base_dir=pr
+rm -rf ${base_dir}
 
+for pr in `git ls-remote |awk '{print $2}'  |grep -v merge |sort -n  -t / -k 3 |tail -n 10 `;do 
+    subdir=`echo $pr|awk -F '/' '{print $3}'`
+    git fetch origin $pr:pr-${subdir}
+    git checkout -- .
+    git checkout pr-${subdir}
+    git pull 
+    make html;mkdir -p ${base_dir}/${subdir}
+    mv build/html/* ${base_dir}/${subdir}
+done
+# Update Master Code
+git checkout master
+git pull 
